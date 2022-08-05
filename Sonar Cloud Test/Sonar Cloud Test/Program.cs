5393135c0a1357d16df236bad54cd43f4e7faf77
James Beitz <108765503+BeitzJR@users.noreply.github.com>
2022-08-05 00:12:00 -0700

    Update Program.cs

--- Sonar Cloud Test/Sonar Cloud Test/Program.cs
+++ Sonar Cloud Test/Sonar Cloud Test/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!!!");
 Console.WriteLine("This Is SPARTA!!");
 Console.WriteLine("I got a bad feeling about this");
 Console.WriteLine("Here's looking at you kid");
+Console.WriteLine("HAIL TO THE FLAIL!!")

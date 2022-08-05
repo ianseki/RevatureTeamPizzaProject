b99855aa62f0ce2315eb40d01ee4b87bbdf49ae3
James Beitz <108765503+BeitzJR@users.noreply.github.com>
2022-08-04 23:12:07 -0700

    Update Program.cs

--- Sonar Cloud Test/Sonar Cloud Test/Program.cs
+++ Sonar Cloud Test/Sonar Cloud Test/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!!");
 Console.WriteLine("This Is SPARTA!!");
+Console.WriteLine("I got a bad feeling about this");

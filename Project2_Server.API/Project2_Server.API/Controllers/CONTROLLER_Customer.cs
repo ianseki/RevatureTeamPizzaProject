af14ddafbba556ad7a0b33cf2c39486a86385ba1
arthurgao697 <arthurgao697@revature.net>
2022-08-10 09:19:00 -0700

    Changed routing for controller

--- Project2_Server.API/Project2_Server.API/Controllers/CONTROLLER_Customer.cs
+++ Project2_Server.API/Project2_Server.API/Controllers/CONTROLLER_Customer.cs
@@ -8,7 +8,7 @@ using System.Security.Cryptography;
 
 namespace Project2_Server.API.Controllers
 {
-    [Route("api/Customer")]
+    [Route("API/Customer")]
     [ApiController]
     public class CONTROLLER_Customer : ControllerBase
     {
@@ -29,7 +29,8 @@ namespace Project2_Server.API.Controllers
             this.API_DATA_Logger = INPUT_Logger;
         }
 
-        [HttpGet("INPUT_Email/INPUT_Password")]
+        [HttpGet]
+        [Route("CheckLogin")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<bool> API_ASYNC_CUSTOMER_checkValidLogin(string INPUT_Email, string INPUT_Password)
@@ -55,6 +56,7 @@ namespace Project2_Server.API.Controllers
         }
 
         [HttpPost]
+        [Route("CreateNewCustomer")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<bool> API_ASYNC_CUSTOMER_createNewCustomer(DMODEL_Customer INPUT_DMODEL_Customer)
         {
@@ -73,9 +75,10 @@ namespace Project2_Server.API.Controllers
             }
         }
 
-        [HttpPost("INPUT_CustomerID")]
+        [HttpPost]
+        [Route("CreateNewOrder")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<bool> API_ASYNC_CUSTOMER_createNewOrder(int INPUT_CustomerID, [FromBody] DTO_ProjectOrder INPUT_DTO_OrderProject)
+        public async Task<bool> API_ASYNC_CUSTOMER_createNewOrder(int INPUT_CustomerID, [FromBody] DTO_OrderProject INPUT_DTO_OrderProject)
         {
             // Adds Order into [Project2].[Order] Table
             int WORK_generatedOrderID = await API_PROP_INTERFACE_Order.ORDER_ASYNC_createNewOrder(INPUT_DTO_OrderProject.INPUT_DMODEL_Order);
@@ -110,7 +113,55 @@ namespace Project2_Server.API.Controllers
                 await API_PROP_INTERFACE_LinkingTable.LINKING_ASYNC_addToOrderProjectLinkingTable(WORK_generatedOrderID, TEMP_ProjectID);
             }
 
+            API_DATA_Logger.LogInformation("EXECUTED: API_ASYNC_CUSTOMER_createNewOrder --> OUTPUT: Successfully created new order {0} for customer {1}", WORK_generatedOrderID, INPUT_CustomerID);
+
             return true;
+        }
+
+        [HttpGet]
+        [Route("GetOrderHistory")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<DTO_OrderProject>> API_ASYNC_CUSTOMER_getOrderHistory(int INPUT_CustomerID)
+        {
+            List<DTO_OrderProject> OUTPUT_DTO_OrderProject = new List<DTO_OrderProject>();
+
+            List<int> WORK_LIST_OrderIDs = new List<int>();
+            WORK_LIST_OrderIDs = await API_PROP_INTERFACE_LinkingTable.LINKING_ASYNC_getFromCustomerOrderLinkingTable(INPUT_CustomerID);
+
+            // Branch where Customer has no previous orders
+            if (WORK_LIST_OrderIDs[0] == -1)
+            {
+                DTO_OrderProject DTO_Blank = new DTO_OrderProject();
+                DMODEL_Order DMODEL_Order_Blank = new DMODEL_Order(-1, DateTime.MinValue, false);
+                DMODEL_Project DMODEL_Project_Blank = new DMODEL_Project(-1, -1, false);
+
+                DTO_Blank.INPUT_DMODEL_Order = DMODEL_Order_Blank;
+                DTO_Blank.INPUT_LIST_DMODEL_Projects.Add(DMODEL_Project_Blank);
+
+                OUTPUT_DTO_OrderProject.Add(DTO_Blank);
+
+                API_DATA_Logger.LogInformation("EXECUTED: API_ASYNC_CUSTOMER_getOrderHistory --> OUTPUT: Customer {0} has no order history, returning blank", INPUT_CustomerID);
+                return OUTPUT_DTO_OrderProject;
+            }
+
+            // Branch where Customer has previous orders
+            for (int i = 0; i < WORK_LIST_OrderIDs.Count; i++)
+            {
+                DTO_OrderProject TEMP_DTO_OrderProject = new DTO_OrderProject();
+                TEMP_DTO_OrderProject.INPUT_DMODEL_Order = await API_PROP_INTERFACE_Order.ORDER_ASYNC_getOrderData(WORK_LIST_OrderIDs[i]);
+
+                List<int> WORK_LIST_ProjectIDs = await API_PROP_INTERFACE_LinkingTable.LINKING_ASYNC_getFromOrderProjectLinkingTable(WORK_LIST_OrderIDs[i]);
+
+                for (int j = 0; j < WORK_LIST_ProjectIDs.Count; j++)
+                {
+                    TEMP_DTO_OrderProject.INPUT_LIST_DMODEL_Projects.Add(await API_PROP_INTERFACE_Project.PROJECT_ASYNC_getProjectData(WORK_LIST_ProjectIDs[j]));
+                }
+
+                OUTPUT_DTO_OrderProject.Add(TEMP_DTO_OrderProject);
+            }
+
+            API_DATA_Logger.LogInformation("EXECUTED: API_ASYNC_CUSTOMER_getOrderHistory --> OUTPUT: found customer {0} order history, returning List of DTOs", INPUT_CustomerID);
+            return OUTPUT_DTO_OrderProject;
         }
     }
 }

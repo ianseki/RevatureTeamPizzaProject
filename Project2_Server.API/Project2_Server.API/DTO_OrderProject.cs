ff9f8eae658f9c285899eda96994c7bf07106dda
arthurgao697 <arthurgao697@revature.net>
2022-08-11 06:28:21 -0700

    Changed DTO syntax to account for both input and output usage

--- Project2_Server.API/Project2_Server.API/DTO_OrderProject.cs
+++ Project2_Server.API/Project2_Server.API/DTO_OrderProject.cs
@@ -5,15 +5,15 @@ namespace Project2_Server.API
     public class DTO_OrderProject
     {
         // FIELDS
-        public DMODEL_Order INPUT_DMODEL_Order { get; set; }
-        public List<DMODEL_Project> INPUT_LIST_DMODEL_Projects { get; set; }
+        public DMODEL_Order DMODEL_Order { get; set; }
+        public List<DMODEL_Project> LIST_DMODEL_Projects { get; set; }
 
         // CONSTRUCTORS
         public DTO_OrderProject() { }
         public DTO_OrderProject(DMODEL_Order INPUT_DMODEL_Order, List<DMODEL_Project> INPUT_LIST_DMODEL_Projects)
         {
-            this.INPUT_DMODEL_Order = INPUT_DMODEL_Order;
-            this.INPUT_LIST_DMODEL_Projects = INPUT_LIST_DMODEL_Projects;
+            this.DMODEL_Order = INPUT_DMODEL_Order;
+            this.LIST_DMODEL_Projects = INPUT_LIST_DMODEL_Projects;
         }
 
         // METHODS

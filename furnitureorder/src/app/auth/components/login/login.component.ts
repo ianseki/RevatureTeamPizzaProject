d438e440abe82c7a9738e689e126e2bfab378c82
EuniceD <eunicedecena@gmail.com>
2022-08-15 14:14:39 -0700

    Customer Portal

---
+++ furnitureorder/src/app/auth/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/furniture/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ furnitureorder/src/app/services/furniture/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  // authUrl = 'https://localhost:7044/API/Customer/CreateNewCustomer';
+
+  constructor() {}
+}
